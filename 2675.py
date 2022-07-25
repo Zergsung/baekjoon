8a68716e4eea3caaf85d17801d31e9d0074f5f9b
sungmo <usungmo0317@gmail.com>
2022-07-25 20:24:25 +0900

    문제별로 설명주석 달아놓음

--- 2675.py
+++ 2675.py
@@ -1,9 +1,9 @@
-retry=int(input())
-for i in range(0,retry):
-    ret, word=input().split()
-    ret=int(ret)
-    list(word)
-    for j in range(1,len(word)+1):
-        for k in range(0,ret):
+retry=int(input()) #반복할 횟수
+for i in range(0,retry): 
+    ret, word=input().split() #각 글자를 몇번 반복할것인지, 반복할 문자
+    ret=int(ret) #숫자는 정수형으로 바꿔주기
+    list(word) #문자는 리스트에 잘라서 넣기
+    for j in range(1,len(word)+1): #총 글자수만큼 반복
+        for k in range(0,ret): #같은글자 입력받은 횟수만큼 출력
             print(word[j-1], end='')
     print()
--- 2908.py
+++ 2908.py
@@ -1,20 +1,20 @@
-num1,num2=input().split()
-num1list=list(str(num1))
-num2list=list(str(num2))
+num1,num2=input().split() #세자리숫자 2개 입력받기
+num1list=list(str(num1)) 
+num2list=list(str(num2)) #각 숫자들 리스트에 넣어주기
 num1lst=[ ]
 num2lst=[ ]
-range=[2,1,0]
+range=[2,1,0] #세자리 숫자 반복할려고 만듬
 
 for i in range:
     num1lst.append(num1list[i])
-    num2lst.append(num2list[i])
+    num2lst.append(num2list[i]) #각 숫자 앞뒤 바꿔주기
 #이부분에서 문자형식의 리스트를 숫자로 바꾸는걸 모르겠어서 찾아봄
 num1lst=list(map(int,num1lst))
-num2lst=list(map(int,num2lst))
+num2lst=list(map(int,num2lst)) 
 
 finnum1=num1lst[0]*100+num1lst[1]*10+num1lst[2]
 finnum2=num2lst[0]*100+num2lst[1]*10+num2lst[2]
-
+#잘라진 숫자 합쳐주기
 if finnum1>finnum2:
     print(finnum1)
 else:
