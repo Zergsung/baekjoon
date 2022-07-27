72e010ac75e537952404dfaedf8bbb610b1acdde
sungmo <usungmo0317@gmail.com>
2022-07-28 00:44:51 +0900

    백준 5622

--- 5622.py
+++ 5622.py
@@ -4,10 +4,10 @@ letterlen=len(letter) #총 몇문자인지 세기
 finres=[] #몇초 걸리는지 기록할 리스트
 
 for i in range(0,letterlen): # 문자 전부 다이얼 몇번에 있는지 알기위해 돌림
-    th=1 #몇번째에 위치하는지 카운트
+    th=2 #몇번째에 위치하는지 카운트
     for j in dial1: #다이얼별로 문자가 포함되어있는지 확인
         if letter[i] in j: #전부확인
-            finres.append(th+2) #2초 더해서 집합에 넣기
+            finres.append(th+1) #1초 더해서 집합에 넣기
         th+=1 #카운트 올리기
 
 print(sum(finres)) #sum 사용해서 리스트값 다 더한값 출력
