1b9c4d30d5dedd66d7566456f04758ca2b7051dd
sungmo0317 <101343432+sungmo0317@users.noreply.github.com>
2022-09-02 20:04:42 +0900

    Update 10809.py

--- 10809.py
+++ 10809.py
@@ -2,4 +2,5 @@ word = input()
 alphabet = list(range(97,123))  # 아스키코드 숫자 범위
 
 for x in alphabet :
-    print(word.find(chr(x))) #알파벳이랑 대칭되는 아스키코드 출력
+    print(word.find(chr(x))) #알파벳이랑 대칭되는 아스키코드 출력하기
+    
