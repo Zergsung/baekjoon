c8fa8c8c0d49597796b735dee77d1ea02cdc21eb
sungmo0317 <usungmo0317@gmail.com>
2023-06-28 19:06:57 +0900

    입출력과 사칙연산 2번 정답

---
+++ rust/1000.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let mut nums_iter = input.split_whitespace();
+
+    let num1: i32 = nums_iter.next().unwrap().parse().unwrap();
+    let num2: i32 = nums_iter.next().unwrap().parse().unwrap();
+
+    println!("{}", num1 + num2);
+}
